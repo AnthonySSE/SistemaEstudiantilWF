b6dbd607b47ebfdae18287780eb026762c020d52
ismaelVF <francoisimael40@gmail.com>
2023-06-01 15:41:29 -0400

    Reporte de estudiantes metodos

---
+++ SistemaEstudiantilWF/Estudiante.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEstudiantilWF
+{
+    internal class Estudiante
+    {
+        public int Id { get; set;  }
+
+        public char Matricula { get; set; }
+
+        public char Nombre { get; set; }
+
+        public char Apellidos { get; set; }
+
+        public int Edad { get; set; }
+
+        public char Sexo { get; set; }
+
+        public  DateTime FechaNacimiento { get; set; }
+
+        public char Curso { get; set; }
+
+        public int CantidadMateria { get; set; }
+
+        public char UsuariId { get; set; }
+    }
+}
