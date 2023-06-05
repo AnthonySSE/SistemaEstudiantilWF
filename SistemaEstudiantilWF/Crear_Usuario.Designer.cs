0b936d0f2229e28cb9757985f569e8c328d9ebbe
JuanF <juanhfamilia@gmail.com>
2023-06-04 22:40:51 -0400

    Agregando tabla de crear usuario

---
+++ SistemaEstudiantilWF/Crear_Usuario.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace SistemaEstudiantilWF
+{
+    partial class Crear_Usuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Crear_Usuario));
+            Barra_Seleccion = new Panel();
+            pictureBox1 = new PictureBox();
+            btnCerrar = new PictureBox();
+            buttonLimpiar = new Button();
+            buttonCrear = new Button();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            Panel = new Panel();
+            btnCrearUsuario = new Button();
+            btnActulizarUsuario = new Button();
+            btnEliminarUsuario = new Button();
+            btnSalir = new Button();
+            btnReporteUsuario = new Button();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            Barra_Seleccion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).BeginInit();
+            Panel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Barra_Seleccion
+            // 
+            Barra_Seleccion.BackColor = Color.RoyalBlue;
+            Barra_Seleccion.Controls.Add(pictureBox1);
+            Barra_Seleccion.Controls.Add(btnCerrar);
+            Barra_Seleccion.Dock = DockStyle.Top;
+            Barra_Seleccion.Location = new Point(0, 0);
+            Barra_Seleccion.Name = "Barra_Seleccion";
+            Barra_Seleccion.Size = new Size(1055, 37);
+            Barra_Seleccion.TabIndex = 45;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.RoyalBlue;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(1027, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(25, 25);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.BackColor = Color.RoyalBlue;
+            btnCerrar.Image = (Image)resources.GetObject("btnCerrar.Image");
+            btnCerrar.Location = new Point(1027, 3);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(25, 25);
+            btnCerrar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnCerrar.TabIndex = 8;
+            btnCerrar.TabStop = false;
+            // 
+            // buttonLimpiar
+            // 
+            buttonLimpiar.BackColor = Color.RoyalBlue;
+            buttonLimpiar.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonLimpiar.ForeColor = Color.White;
+            buttonLimpiar.Location = new Point(875, 435);
+            buttonLimpiar.Name = "buttonLimpiar";
+            buttonLimpiar.Size = new Size(160, 35);
+            buttonLimpiar.TabIndex = 66;
+            buttonLimpiar.Text = "Limpiar";
+            buttonLimpiar.UseVisualStyleBackColor = false;
+            // 
+            // buttonCrear
+            // 
+            buttonCrear.BackColor = Color.RoyalBlue;
+            buttonCrear.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonCrear.ForeColor = Color.White;
+            buttonCrear.Location = new Point(333, 435);
+            buttonCrear.Name = "buttonCrear";
+            buttonCrear.Size = new Size(160, 35);
+            buttonCrear.TabIndex = 56;
+            buttonCrear.Text = "  Crear";
+            buttonCrear.UseVisualStyleBackColor = false;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(778, 92);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "   Apellido";
+            textBox4.Size = new Size(177, 23);
+            textBox4.TabIndex = 65;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(788, 60);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 19);
+            label4.TabIndex = 64;
+            label4.Text = "Apellido";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(567, 186);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "   Cargo";
+            textBox3.Size = new Size(177, 23);
+            textBox3.TabIndex = 63;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(567, 154);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 19);
+            label3.TabIndex = 62;
+            label3.Text = "Cargo";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(567, 92);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "  Nombre";
+            textBox2.Size = new Size(177, 23);
+            textBox2.TabIndex = 61;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(567, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 19);
+            label2.TabIndex = 60;
+            label2.Text = "Nombre";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(308, 92);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "  ID";
+            textBox1.Size = new Size(215, 23);
+            textBox1.TabIndex = 59;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(308, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 19);
+            label1.TabIndex = 58;
+            label1.Text = "ID";
+            // 
+            // Panel
+            // 
+            Panel.BackColor = Color.WhiteSmoke;
+            Panel.Controls.Add(btnCrearUsuario);
+            Panel.Controls.Add(btnActulizarUsuario);
+            Panel.Controls.Add(btnEliminarUsuario);
+            Panel.Controls.Add(btnSalir);
+            Panel.Controls.Add(btnReporteUsuario);
+            Panel.Dock = DockStyle.Left;
+            Panel.Location = new Point(0, 37);
+            Panel.Name = "Panel";
+            Panel.Size = new Size(261, 449);
+            Panel.TabIndex = 57;
+            // 
+            // btnCrearUsuario
+            // 
+            btnCrearUsuario.BackColor = Color.RoyalBlue;
+            btnCrearUsuario.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnCrearUsuario.ForeColor = Color.White;
+            btnCrearUsuario.Location = new Point(3, 23);
+            btnCrearUsuario.Name = "btnCrearUsuario";
+            btnCrearUsuario.Size = new Size(248, 35);
+            btnCrearUsuario.TabIndex = 14;
+            btnCrearUsuario.Text = "Crear Usuario";
+            btnCrearUsuario.UseVisualStyleBackColor = false;
+            // 
+            // btnActulizarUsuario
+            // 
+            btnActulizarUsuario.BackColor = Color.RoyalBlue;
+            btnActulizarUsuario.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnActulizarUsuario.ForeColor = Color.White;
+            btnActulizarUsuario.Location = new Point(3, 70);
+            btnActulizarUsuario.Name = "btnActulizarUsuario";
+            btnActulizarUsuario.Size = new Size(248, 35);
+            btnActulizarUsuario.TabIndex = 15;
+            btnActulizarUsuario.Text = "Actualizar Usuario";
+            btnActulizarUsuario.UseVisualStyleBackColor = false;
+            // 
+            // btnEliminarUsuario
+            // 
+            btnEliminarUsuario.BackColor = Color.RoyalBlue;
+            btnEliminarUsuario.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnEliminarUsuario.ForeColor = Color.White;
+            btnEliminarUsuario.Location = new Point(3, 117);
+            btnEliminarUsuario.Name = "btnEliminarUsuario";
+            btnEliminarUsuario.Size = new Size(248, 35);
+            btnEliminarUsuario.TabIndex = 16;
+            btnEliminarUsuario.Text = "Eliminar Usuario";
+            btnEliminarUsuario.UseVisualStyleBackColor = false;
+            // 
+            // btnSalir
+            // 
+            btnSalir.BackColor = Color.RoyalBlue;
+            btnSalir.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSalir.ForeColor = Color.White;
+            btnSalir.Location = new Point(12, 398);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(177, 30);
+            btnSalir.TabIndex = 12;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = false;
+            // 
+            // btnReporteUsuario
+            // 
+            btnReporteUsuario.BackColor = Color.RoyalBlue;
+            btnReporteUsuario.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnReporteUsuario.ForeColor = Color.White;
+            btnReporteUsuario.Location = new Point(3, 164);
+            btnReporteUsuario.Name = "btnReporteUsuario";
+            btnReporteUsuario.Size = new Size(248, 35);
+            btnReporteUsuario.TabIndex = 17;
+            btnReporteUsuario.Text = "Reporte de Usuario";
+            btnReporteUsuario.UseVisualStyleBackColor = false;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(308, 186);
+            textBox5.Name = "textBox5";
+            textBox5.PlaceholderText = "   Usuario";
+            textBox5.Size = new Size(215, 23);
+            textBox5.TabIndex = 68;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(308, 154);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 19);
+            label5.TabIndex = 67;
+            label5.Text = "Usuario";
+            // 
+            // Crear_Usuario
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1055, 486);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(buttonLimpiar);
+            Controls.Add(buttonCrear);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(Panel);
+            Controls.Add(Barra_Seleccion);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Crear_Usuario";
+            Text = "Crear_Usuario";
+            Barra_Seleccion.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).EndInit();
+            Panel.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel Barra_Seleccion;
+        private PictureBox pictureBox1;
+        private PictureBox btnCerrar;
+        private Button buttonLimpiar;
+        private Button buttonCrear;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label1;
+        private Panel Panel;
+        private Button btnCrearUsuario;
+        private Button btnActulizarUsuario;
+        private Button btnEliminarUsuario;
+        private Button btnSalir;
+        private Button btnReporteUsuario;
+        private TextBox textBox5;
+        private Label label5;
+    }
+}
