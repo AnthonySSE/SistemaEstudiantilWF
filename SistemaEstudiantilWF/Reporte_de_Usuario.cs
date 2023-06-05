d4599c0b68cf03808dc60152fc984f68deba5b25
JuanF <juanhfamilia@gmail.com>
2023-06-05 12:10:07 -0400

    Agregando tabla de Reporte de usuario

---
+++ SistemaEstudiantilWF/Reporte_de_Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaEstudiantilWF
+{
+    public partial class Reporte_de_Usuario : Form
+    {
+        public Reporte_de_Usuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
